8e6082d58817515a6bb6cbcf24b33ea98cbda323
vincentStarck <vincent.starck@gmail.com>
2017-05-14 21:43:09 -0500

    add application.yml file

--- src/main/java/vincent/starck/projects/model/User.java
+++ src/main/java/vincent/starck/projects/model/User.java
@@ -5,36 +5,65 @@ import org.springframework.data.annotation.Id;
 public class User {
 
 	@Id
-	public String id;
-	public String firstName;
-	public String lastName;
-	public String middleName;
+	private String id;
+	private String firstName;
+	private String lastName;
+	private String middleName;
+	private String email;
 	
+	
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	/**
+	 * @return the middleName
+	 */
 	public String getMiddleName() {
 		return middleName;
 	}
-
+	/**
+	 * @param middleName the middleName to set
+	 */
 	public void setMiddleName(String middleName) {
 		this.middleName = middleName;
 	}
-
+	/**
+	 * @return the email
+	 */
 	public String getEmail() {
 		return email;
 	}
-
+	/**
+	 * @param email the email to set
+	 */
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
-	public String email;
 	
 
-	public User() {
-	}
 
-	public User(String firstName, String lastName) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-	}
+
 
 }
