aa695a584b14090cceca589478ce140c399a8808
vincentStarck <vincent.starck@gmail.com>
2017-05-23 10:39:42 -0500

    update UserController.js

--- src/main/java/vincent/starck/projects/controller/UserController.java
+++ src/main/java/vincent/starck/projects/controller/UserController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,12 +24,14 @@ public class UserController {
 
 	@Autowired
 	private UsuarioService repository;
-
+   
+	@CrossOrigin
 	@RequestMapping(value = "/newUser", method = RequestMethod.POST)
-	public @ResponseBody void createUser(@RequestBody User user) {
+	public @ResponseBody User createUser(@RequestBody User user) {
 		LOGGER.info("Add new user");
 		repository.save(user);
 		LOGGER.info("Add new user ok: \n{}", new Gson().toJson(user));
+		return user;
 
 	}
 
